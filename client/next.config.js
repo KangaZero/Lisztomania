b6de15f0a6afce129fb7d1c45a9802fe67ee0f70
Samuel <samuelyongw@gmail.com>
2023-03-29 02:13:01 +1100

    Initialised Next js project with typescript and included PWA webpack to make it installable

---
+++ client/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA({
+  ...nextConfig,
+})
